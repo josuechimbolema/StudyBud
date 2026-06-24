13b84bb1765530efcf9d7fbb34dfdfa6e09497e4
josue <jacobojosue2@gmail.com>
2026-06-23 21:40:40 -0500

    ci-setup: agregar GitHub Actions y tests

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+class BasicTests(SimpleTestCase):
+    
+    def test_suma_basica(self):
+        self.assertEqual(1 + 1, 2)
+    
+    def test_string(self):
+        self.assertEqual("django".upper(), "DJANGO")
+    
+    def test_lista(self):
+        lista = [1, 2, 3]
+        self.assertIn(2, lista)
